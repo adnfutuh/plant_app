fdb6fd0ffa90cbe236bc3eb157b1714b3fe48b2f
rizki <rizkiadnanfutuh@gmail.com>
2024-09-27 02:08:54 +0700

    add colors

--- lib/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeColors {
-  static const white = Color(0xFFFFFFFF);
-  static const green = Color(0xFF55AA9C);
-  static const green2 = Color(0xFFD1EAC0);
-  static const grey = Color(0xFFE8E8E8);
-  static const black = Color(0xFF0E0E0E);
-}
---
+++ lib/const/colors.dart
@@ -0,0 +1,8 @@
+import "dart:ui";
+
+const white = Color(0xFFF8F8F8);
+const green = Color(0xFF55AA9C);
+const green2 = Color(0xFFD1EAC0);
+const grey1 = Color(0xFFE8E8E8);
+const grey2 = Color(0xFF4A4543);
+const black = Color(0xFF0E0E0E);
