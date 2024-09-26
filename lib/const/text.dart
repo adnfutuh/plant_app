b56271cbc46b69c00f18981ef38daca051aeeacc
rizki <rizkiadnanfutuh@gmail.com>
2024-09-27 02:09:38 +0700

    add text

---
+++ lib/const/text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+TextStyle regular = const TextStyle(
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle medium = const TextStyle(
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle semiBold = const TextStyle(
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle bold = const TextStyle(
+  fontWeight: FontWeight.w700,
+);
