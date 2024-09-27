cfac10801494843b0544809c80c9d5fa7e61c9b7
rizki <rizkiadnanfutuh@gmail.com>
2024-09-27 07:48:24 +0700

    call all sections

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/screens/sections/home_appbar.dart';
+import 'package:plant_app/screens/sections/home_header.dart';
+import 'package:plant_app/screens/sections/home_recommended.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: HomeAppbar(),
+      body: Column(
+        children: [
+          HomeHeader(),
+          SizedBox(height: 30),
+          HomeRecommended(),
+        ],
+      ),
+    );
+  }
+}
