cfd2e0accf68ffb461f923561d6de3bfdac48475
rizki <rizkiadnanfutuh@gmail.com>
2024-09-27 07:47:23 +0700

    add home_recommeded

---
+++ lib/screens/sections/home_recommended.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/screens/components/home_card.dart';
+
+import '../../const/text.dart';
+
+class HomeRecommended extends StatelessWidget {
+  const HomeRecommended({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Text(
+            'Recommended Plants',
+            style: medium.copyWith(fontSize: 22),
+          ),
+        ),
+        const SizedBox(height: 30),
+        const Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: SizedBox(
+            height: 380,
+            child: HomeCard(),
+          ),
+        )
+      ],
+    );
+  }
+}
